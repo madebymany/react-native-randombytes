7fc0919e81dc06168ee07bb1d4503fb26abc2f01
Dan Brown <dan@madebymany.co.uk>
2016-08-09 16:19:20 +0100

    Removed sync interface and sjcl

--- index.js
+++ index.js
@@ -2,46 +2,10 @@ if (typeof Buffer === 'undefined') {
   global.Buffer = require('buffer').Buffer
 }
 
-let sjcl = require('sjcl')
 let RNRandomBytes = require('react-native').NativeModules.RNRandomBytes
-
-function noop () {}
 
 function toBuffer (nativeStr) {
   return new Buffer(nativeStr, 'base64')
-}
-
-function init () {
-  if (RNRandomBytes.seed) {
-    let seedBuffer = toBuffer(RNRandomBytes.seed)
-    addEntropy(seedBuffer)
-  } else {
-    seedSJCL()
-  }
-}
-
-function addEntropy (entropyBuf) {
-  let hexString = entropyBuf.toString('hex')
-  let stanfordSeed = sjcl.codec.hex.toBits(hexString)
-  sjcl.random.addEntropy(stanfordSeed)
-}
-
-export function seedSJCL (cb) {
-  cb = cb || noop
-  randomBytes(4096, function (err, buffer) {
-    if (err) return cb(err)
-
-    addEntropy(buffer)
-  })
-}
-
-export function randomBytesSync (length) {
-  let size = length
-  let wordCount = Math.ceil(size * 0.25)
-  let randomBytes = sjcl.random.randomWords(wordCount, 10)
-  let hexString = sjcl.codec.hex.fromBits(randomBytes)
-  hexString = hexString.substr(0, size * 2)
-  return new Buffer(hexString, 'hex')
 }
 
 export function randomBytes (length) {
@@ -55,5 +19,3 @@ export function randomBytes (length) {
     })
   })
 }
-
-init()
